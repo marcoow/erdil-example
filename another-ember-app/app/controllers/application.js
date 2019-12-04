e4386de56b371f857a290a7039079c690f002971
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-12-04 12:28:18 +0100

    single sign in/out

---
+++ another-ember-app/app/controllers/application.js
@@ -0,0 +1,30 @@
+import Controller from '@ember/controller';
+import { inject } from '@ember/service';
+
+export default Controller.extend({
+  session: inject(),
+
+  actions: {
+    login() {
+      this.session.authenticate('authenticator:oauth2', 'username', 'password');
+    },
+
+    logout() {
+      this.session.invalidate();
+    },
+
+    async load() {
+      let response = await fetch('http://localhost:3001/sample-data', {
+        headers: {
+          Authorization: `Bearer ${this.session.data.authenticated.access_token}`
+        }
+      });
+      if (response.status === 401) {
+        this.session.invalidate();
+      } else {
+        let data = await response.text();
+        this.set('data', data);
+      }
+    }
+  }
+});
